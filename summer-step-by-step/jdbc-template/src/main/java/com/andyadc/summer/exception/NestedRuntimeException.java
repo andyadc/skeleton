0c4a819b5e89912d3df6eacb5b4250eefd509ad3
jjwadc <jjwadc@gmail.com>
2025-05-12 09:36:44 +0800

    summer step | jdbc template

---
+++ summer-step-by-step/jdbc-template/src/main/java/com/andyadc/summer/exception/NestedRuntimeException.java
@@ -0,0 +1,20 @@
+package com.andyadc.summer.exception;
+
+public class NestedRuntimeException extends RuntimeException {
+
+    public NestedRuntimeException() {
+    }
+
+    public NestedRuntimeException(String message) {
+        super(message);
+    }
+
+    public NestedRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public NestedRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
