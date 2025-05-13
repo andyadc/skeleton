0e8029eb1ce3d8426b61405629b0f322a3ba3cf9
jjwadc <jjwadc@gmail.com>
2025-05-13 08:42:41 +0800

    summer step | jdbc template

---
+++ summer-step-by-step/jdbc-template/src/main/java/com/andyadc/summer/jdbc/PreparedStatementCreator.java
@@ -0,0 +1,12 @@
+package com.andyadc.summer.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface PreparedStatementCreator {
+
+    PreparedStatement createPreparedStatement(Connection connection) throws SQLException;
+
+}
