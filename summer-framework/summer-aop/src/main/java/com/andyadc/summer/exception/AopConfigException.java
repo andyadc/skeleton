fdedf72031610e2fed821c924fa886548cde8389
jjwadc <jjwadc@gmail.com>
2025-05-14 10:07:50 +0800

    summer aop

---
+++ summer-framework/summer-aop/src/main/java/com/andyadc/summer/exception/AopConfigException.java
@@ -0,0 +1,21 @@
+package com.andyadc.summer.exception;
+
+public class AopConfigException extends NestedRuntimeException {
+
+    public AopConfigException() {
+        super();
+    }
+
+    public AopConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AopConfigException(String message) {
+        super(message);
+    }
+
+    public AopConfigException(Throwable cause) {
+        super(cause);
+    }
+
+}
