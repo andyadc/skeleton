d7647a81c16b83314df8ca08a5acce9b1da19730
jjwadc <jjwadc@gmail.com>
2025-05-20 08:57:56 +0800

    summer aop

---
+++ summer-framework/summer-aop/src/main/java/com/andyadc/summer/aop/AroundProxyBeanPostProcessor.java
@@ -0,0 +1,10 @@
+package com.andyadc.summer.aop;
+
+import com.andyadc.summer.annotation.Around;
+
+/**
+ * Create proxy for @Around.
+ */
+public class AroundProxyBeanPostProcessor extends AnnotationProxyBeanPostProcessor<Around> {
+
+}
