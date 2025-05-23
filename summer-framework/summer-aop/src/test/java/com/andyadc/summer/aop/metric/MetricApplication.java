f51ed18f12662d803ecb4d00c1c89d66216fe757
jjwadc <jjwadc@gmail.com>
2025-05-23 09:04:34 +0800

    summer aop test

---
+++ summer-framework/summer-aop/src/test/java/com/andyadc/summer/aop/metric/MetricApplication.java
@@ -0,0 +1,10 @@
+package com.andyadc.summer.aop.metric;
+
+import com.andyadc.summer.annotation.ComponentScan;
+import com.andyadc.summer.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class MetricApplication {
+
+}
---
+++ summer-framework/summer-aop/src/test/java/com/andyadc/summer/aop/metric/MetricProxyBeanPostProcessor.java
@@ -0,0 +1,9 @@
+package com.andyadc.summer.aop.metric;
+
+import com.andyadc.summer.annotation.Component;
+import com.andyadc.summer.aop.AnnotationProxyBeanPostProcessor;
+
+@Component
+public class MetricProxyBeanPostProcessor extends AnnotationProxyBeanPostProcessor<Metric> {
+
+}
