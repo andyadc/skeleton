60d3baa3cd5d9d70cbfbb81db004b07ca8a97eb3
jjwadc <jjwadc@gmail.com>
2025-05-26 09:54:35 +0800

    summer web

---
+++ summer-framework/summer-web/src/main/java/com/andyadc/summer/web/View.java
@@ -0,0 +1,16 @@
+package com.andyadc.summer.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Map;
+
+public interface View {
+
+    default String getContentType() {
+        return null;
+    }
+
+    void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+}
