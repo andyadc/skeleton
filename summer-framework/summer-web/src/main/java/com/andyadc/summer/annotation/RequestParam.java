977882a0dc831d22f837e60c8eddd00398e16047
jjwadc <jjwadc@gmail.com>
2025-05-29 08:43:19 +0800

    summer web controller

---
+++ summer-framework/summer-web/src/main/java/com/andyadc/summer/annotation/RequestParam.java
@@ -0,0 +1,20 @@
+package com.andyadc.summer.annotation;
+
+import com.andyadc.summer.web.utils.WebUtils;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RequestParam {
+
+    String value();
+
+    String defaultValue() default WebUtils.DEFAULT_PARAM_VALUE;
+
+}
