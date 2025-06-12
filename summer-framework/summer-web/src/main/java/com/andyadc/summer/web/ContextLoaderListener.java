5f8b060137e75638d13798590e057e1960e3bf38
jjwadc <jjwadc@gmail.com>
2025-06-12 09:07:17 +0800

    summer web

--- summer-framework/summer-web/src/main/java/com/andyadc/summer/web/ContextLoaderListener.java
+++ summer-framework/summer-web/src/main/java/com/andyadc/summer/web/ContextLoaderListener.java
@@ -1,6 +1,10 @@
 package com.andyadc.summer.web;
 
-import jakarta.servlet.ServletContext;
+import com.andyadc.summer.context.AnnotationConfigApplicationContext;
+import com.andyadc.summer.context.ApplicationContext;
+import com.andyadc.summer.exception.NestedRuntimeException;
+import com.andyadc.summer.io.PropertyResolver;
+import com.andyadc.summer.web.utils.WebUtils;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import org.slf4j.Logger;
@@ -13,10 +17,34 @@ public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         logger.info("init {}.", getClass().getName());
-        ServletContext servletContext = sce.getServletContext();
+        var servletContext = sce.getServletContext();
         WebMvcConfiguration.setServletContext(servletContext);
 
+        var propertyResolver = WebUtils.createPropertyResolver();
+        String encoding = propertyResolver.getProperty("${summer.web.character-encoding:UTF-8}");
+        servletContext.setRequestCharacterEncoding(encoding);
+        servletContext.setResponseCharacterEncoding(encoding);
+        var applicationContext = createApplicationContext(servletContext.getInitParameter("configuration"), propertyResolver);
+        // register filters:
+        WebUtils.registerFilters(servletContext);
+        // register DispatcherServlet:
+        WebUtils.registerDispatcherServlet(servletContext, propertyResolver);
 
+        servletContext.setAttribute("applicationContext", applicationContext);
+    }
+
+    ApplicationContext createApplicationContext(String configClassName, PropertyResolver propertyResolver) {
+        logger.info("init ApplicationContext by configuration: {}", configClassName);
+        if (configClassName == null || configClassName.isEmpty()) {
+            throw new NestedRuntimeException("Cannot init ApplicationContext for missing init param name: configuration");
+        }
+        Class<?> configClass;
+        try {
+            configClass = Class.forName(configClassName);
+        } catch (ClassNotFoundException e) {
+            throw new NestedRuntimeException("Could not load class from init param 'configuration': " + configClassName);
+        }
+        return new AnnotationConfigApplicationContext(configClass, propertyResolver);
     }
 
 }
