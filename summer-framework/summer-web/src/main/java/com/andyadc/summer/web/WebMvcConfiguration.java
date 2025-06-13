b3493a1855507d43869de796c247f2909f7d64cf
jjwadc <jjwadc@gmail.com>
2025-06-13 09:01:09 +0800

    summer web

--- summer-framework/summer-web/src/main/java/com/andyadc/summer/web/WebMvcConfiguration.java
+++ summer-framework/summer-web/src/main/java/com/andyadc/summer/web/WebMvcConfiguration.java
@@ -1,7 +1,12 @@
 package com.andyadc.summer.web;
 
+import com.andyadc.summer.annotation.Autowired;
+import com.andyadc.summer.annotation.Bean;
 import com.andyadc.summer.annotation.Configuration;
+import com.andyadc.summer.annotation.Value;
 import jakarta.servlet.ServletContext;
+
+import java.util.Objects;
 
 @Configuration
 public class WebMvcConfiguration {
@@ -13,6 +18,19 @@ public class WebMvcConfiguration {
      */
     static void setServletContext(ServletContext ctx) {
         servletContext = ctx;
+    }
+
+    @Bean(initMethod = "init")
+    ViewResolver viewResolver( //
+                               @Autowired ServletContext servletContext, //
+                               @Value("${summer.web.freemarker.template-path:/WEB-INF/templates}") String templatePath, //
+                               @Value("${summer.web.freemarker.template-encoding:UTF-8}") String templateEncoding) {
+        return new FreeMarkerViewResolver(servletContext, templatePath, templateEncoding);
+    }
+
+    @Bean
+    ServletContext servletContext() {
+        return Objects.requireNonNull(servletContext, "ServletContext is not set.");
     }
 
 }
