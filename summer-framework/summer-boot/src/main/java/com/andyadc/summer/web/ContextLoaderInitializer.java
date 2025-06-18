2782090262b4e96e6d45fec98ba8f6fdb3977b9a
jjwadc <jjwadc@gmail.com>
2025-06-18 09:16:35 +0800

    summer boot tomcat

--- summer-framework/summer-boot/src/main/java/com/andyadc/summer/web/ContextLoaderInitializer.java
+++ summer-framework/summer-boot/src/main/java/com/andyadc/summer/web/ContextLoaderInitializer.java
@@ -1,4 +1,45 @@
 package com.andyadc.summer.web;
 
-public class ContextLoaderInitializer {
+import com.andyadc.summer.context.AnnotationConfigApplicationContext;
+import com.andyadc.summer.context.ApplicationContext;
+import com.andyadc.summer.io.PropertyResolver;
+import com.andyadc.summer.web.utils.WebUtils;
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+public class ContextLoaderInitializer implements ServletContainerInitializer {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
+    final Class<?> configClass;
+    final PropertyResolver propertyResolver;
+
+    public ContextLoaderInitializer(Class<?> configClass, PropertyResolver propertyResolver) {
+        this.configClass = configClass;
+        this.propertyResolver = propertyResolver;
+    }
+
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext ctx) throws ServletException {
+        logger.info("Servlet container start. ServletContext = {}", ctx);
+
+        String encoding = propertyResolver.getProperty("${summer.web.character-encoding:UTF-8}");
+        ctx.setRequestCharacterEncoding(encoding);
+        ctx.setResponseCharacterEncoding(encoding);
+
+        WebMvcConfiguration.setServletContext(ctx);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(this.configClass, this.propertyResolver);
+        logger.info("Application context created: {}", applicationContext);
+
+        // register filters:
+        WebUtils.registerFilters(ctx);
+        // register DispatcherServlet:
+        WebUtils.registerDispatcherServlet(ctx, this.propertyResolver);
+    }
+
 }
