6051e4cf7c46801464b70b2c5b3cb513b34230f2
andaicheng <andaicheng@gmail.com>
2025-07-06 19:25:23 +0800

    opt

--- summer-framework/summer-context/src/main/java/com/andyadc/summer/exception/NestedRuntimeException.java
+++ summer-framework/summer-context/src/main/java/com/andyadc/summer/exception/NestedRuntimeException.java
@@ -1,6 +1,11 @@
 package com.andyadc.summer.exception;
 
+import java.io.Serial;
+
 public class NestedRuntimeException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -4748330045161022996L;
 
     public NestedRuntimeException() {
     }
