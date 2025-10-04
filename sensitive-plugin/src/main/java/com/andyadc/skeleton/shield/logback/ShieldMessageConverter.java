4342baac6b1b44d971f07d9c1c98c42dee22d748
andaicheng <andaicheng@gmail.com>
2025-10-04 08:19:54 +0800

    sensitive

--- sensitive-plugin/src/main/java/com/andyadc/skeleton/shield/logback/ShieldMessageConverter.java
+++ sensitive-plugin/src/main/java/com/andyadc/skeleton/shield/logback/ShieldMessageConverter.java
@@ -2,6 +2,10 @@ package com.andyadc.skeleton.shield.logback;
 
 import ch.qos.logback.classic.pattern.MessageConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.andyadc.skeleton.shield.utils.ReflectUtils;
+import org.slf4j.helpers.MessageFormatter;
+
+import java.util.stream.Stream;
 
 /**
  * 脱敏转换器
@@ -11,7 +15,11 @@ public class ShieldMessageConverter extends MessageConverter {
     @Override
     public String convert(ILoggingEvent event) {
         if (event.getArgumentArray() != null) {
-
+            return MessageFormatter.arrayFormat(
+                    event.getMessage(),
+                    Stream.of(event.getArgumentArray())
+                            .map(ReflectUtils::reflectToLogStringByFields).toArray()
+            ).getMessage();
         }
         return super.convert(event);
     }
