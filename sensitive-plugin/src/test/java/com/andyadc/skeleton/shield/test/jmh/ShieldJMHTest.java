aeb2d2c2d6f15e242832798f0686f399c4918c37
andaicheng <andaicheng@gmail.com>
2025-10-13 09:39:35 +0800

    sensitive

--- sensitive-plugin/src/test/java/com/andyadc/skeleton/shield/test/jmh/ShieldJMHTest.java
+++ sensitive-plugin/src/test/java/com/andyadc/skeleton/shield/test/jmh/ShieldJMHTest.java
@@ -1,9 +1,16 @@
 package com.andyadc.skeleton.shield.test.jmh;
 
+import com.alibaba.fastjson2.JSON;
 import com.andyadc.skeleton.shield.test.model.TestABClass;
 import com.andyadc.skeleton.shield.test.model.TestAClass;
 import com.andyadc.skeleton.shield.test.model.TestBClass;
+import com.andyadc.skeleton.shield.utils.ReflectUtils;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.concurrent.TimeUnit;
 
@@ -38,6 +45,42 @@ public class ShieldJMHTest {
         bClass.setBlong(1234567890L);
         bClass.setC("CCCCCCCCCCCCCCCCCCCCCCCC");
 
+    }
+
+    @Param(value = {"100", "10000", "1000000"})
+    private int length;
+
+    public static void main(String[] args) throws RunnerException {
+        Options opts = new OptionsBuilder().include(ShieldJMHTest.class.getSimpleName()).resultFormat(ResultFormatType.JSON).build();
+        new Runner(opts).run();
+    }
+
+    @Benchmark
+    public void shieldUtilsTest() {
+        for (int i = 0; i < length; i++) {
+            ReflectUtils.reflectToLogStringByFields(aClass);
+        }
+    }
+
+    @Benchmark
+    public void toStringTest() {
+        for (int i = 0; i < length; i++) {
+            aClass.toString();
+        }
+    }
+
+    @Benchmark
+    public void jsonTest() {
+        for (int i = 0; i < length; i++) {
+            JSON.toJSONString(aClass);
+        }
+    }
+
+    @Benchmark
+    public void noShieldUtilsTest() {
+        for (int i = 0; i < length; i++) {
+            ReflectUtils.reflectToLogStringByFields(bClass);
+        }
     }
 
 }
