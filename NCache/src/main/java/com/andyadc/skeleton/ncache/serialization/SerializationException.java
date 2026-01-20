eae19f76ad2326b6a746d453dad3260ebf71a5b1
andaicheng <andaicheng@gmail.com>
2026-01-20 16:34:13 +0800

    NCache serializer

---
+++ NCache/src/main/java/com/andyadc/skeleton/ncache/serialization/SerializationException.java
@@ -0,0 +1,13 @@
+package com.andyadc.skeleton.ncache.serialization;
+
+public class SerializationException extends RuntimeException {
+
+    public SerializationException(String message) {
+        super(message);
+    }
+
+    public SerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
